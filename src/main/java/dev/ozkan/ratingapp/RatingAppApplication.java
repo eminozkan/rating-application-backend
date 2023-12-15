87c8dd7af1c4f99fadf7d0ae5e3e6172e6792dc2
eminozkan <info@eminozkan.net>
2023-12-15 14:48:37 +0300

    Add Spring boot project files

---
+++ src/main/java/dev/ozkan/ratingapp/RatingAppApplication.java
@@ -0,0 +1,13 @@
+package dev.ozkan.ratingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingAppApplication.class, args);
+	}
+
+}
