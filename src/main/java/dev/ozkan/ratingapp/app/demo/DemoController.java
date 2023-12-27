e60886744ed920dd0fa2e1a5e258ec34c6344f0b
eminozkan <info@eminozkan.net>
2023-12-27 22:56:00 +0300

    Change application to jwt authentication

--- src/main/java/dev/ozkan/ratingapp/app/demo/DemoController.java
+++ src/main/java/dev/ozkan/ratingapp/app/demo/DemoController.java
@@ -1,4 +1,15 @@
 package dev.ozkan.ratingapp.app.demo;
 
+import dev.ozkan.ratingapp.config.UserSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/demo/hello")
+    public ResponseEntity<?> sayHello(UserSession session){
+        return ResponseEntity.ok(session.id() + session.username());
+    }
 }
