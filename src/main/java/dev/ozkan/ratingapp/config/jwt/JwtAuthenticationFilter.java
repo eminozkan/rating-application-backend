b0bc9a9e00c0ca0cb6ede698155c7f7bafad5b3e
eminozkan <info@eminozkan.net>
2024-01-14 02:24:44 +0300

    Add ExpiredJwtException handling to JwtAuthenticationFilter

--- src/main/java/dev/ozkan/ratingapp/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/dev/ozkan/ratingapp/config/jwt/JwtAuthenticationFilter.java
@@ -5,10 +5,12 @@ import dev.ozkan.ratingapp.core.jwt.JwtService;
 import dev.ozkan.ratingapp.core.model.user.User;
 import dev.ozkan.ratingapp.core.model.user.UserRole;
 import dev.ozkan.ratingapp.repository.UserRepository;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Map;
 
 
 @Component
@@ -37,39 +40,46 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader= request.getHeader("Authorization");
-        final String jwt;
-        final String userEmail;
-        if(authHeader == null || !authHeader.startsWith("Bearer ")){
-            filterChain.doFilter(request,response);
-            return ;
-        }
+        try{
+            final String authHeader= request.getHeader("Authorization");
+            final String jwt;
+            final String userEmail;
+            if(authHeader == null || !authHeader.startsWith("Bearer ")){
+                filterChain.doFilter(request,response);
+                return ;
+            }
 
-        jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);
-        UserRole role = UserRole.ROLE_USER;
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            var user = (User) userDetails;
-            if (user.getRole() == UserRole.ROLE_ADMIN){
-                role = UserRole.ROLE_ADMIN;
+            jwt = authHeader.substring(7);
+            userEmail = jwtService.extractUsername(jwt);
+            UserRole role = UserRole.ROLE_USER;
+            if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+                var user = (User) userDetails;
+                if (user.getRole() == UserRole.ROLE_ADMIN){
+                    role = UserRole.ROLE_ADMIN;
+                }
+                if(jwtService.isTokenValid(jwt,userDetails)){
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
-            if(jwtService.isTokenValid(jwt,userDetails)){
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
+            String userId = userRepository.getByEmail(userEmail).orElseThrow().getUserId();
+            var session = new UserSession(userId,userEmail,role);
+            request.setAttribute("SESSION",session);
+            filterChain.doFilter(request,response);
+        }catch (ExpiredJwtException ex){
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.getWriter().write("""
+                    { "message" : "Session has expired" }
+                    """);
         }
-        String userId = userRepository.getByEmail(userEmail).orElseThrow().getUserId();
-        var session = new UserSession(userId,userEmail,role);
-        request.setAttribute("SESSION",session);
-        filterChain.doFilter(request,response);
     }
 
 }
